a8e2a4351a8443eaba0f0434bac37397914f2227
Steve Conger <spconger@gmail.com>
2015-02-10 12:17:00 -0800

    Commit2102015

---
+++ Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    CommunityServicesReference.CAServiceClient csr = new CommunityServicesReference.CAServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if(!IsPostBack)
+        { 
+            List<string> servicesList = csr.GetCommunityServices().ToList();
+            DropDownList1.DataSource = servicesList;
+            DropDownList1.DataBind();
+        }
+    }
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string serv = DropDownList1.SelectedItem.Text;
+        List<CommunityServicesReference.ServiceGrant> grants = csr.GetGrants(serv).ToList();
+        GridView1.DataSource = grants;
+        GridView1.DataBind();
+    }
+}
